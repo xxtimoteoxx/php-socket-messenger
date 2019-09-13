24f2a660ba943705166cfb12d4bddadcc290fb2d
angelozi <hamideyuce@gmail.com>
2019-09-13 11:41:29 +0300

    0.1.0
    Create server and client

--- client.php
+++ client.php
@@ -1,36 +1,359 @@
-<?php
-// no Time Limit
-set_time_limit(0);
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='UTF-8' />
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
-//Set variables such as "host" and "port"
-$host = '127.0.0.1';
-$port = 5353; // Here the port and host should be same as defined in server.
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<style>
+    * {
+        box-sizing: border-box;
+    }
 
-// Create socket
-$socket = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
-if(!$socket) {
-    echo "socket_create() Fail to create:".socket_strerror($socket)."\n";
-}
+    body {
+        background-color: #edeff2;
+        font-family: "Calibri", "Roboto", sans-serif;
+    }
 
-// Connect to the server
-$result = socket_connect($socket, $host, $port);
-if(!$result) {
-    echo "socket_connect() Fail to connect:".socket_strerror($result)."\n";
-}
+    .chat_window {
+        position: absolute;
+        width: calc(100% - 20px);
+        max-width: 800px;
+        height: 500px;
+        border-radius: 10px;
+        background-color: #fff;
+        left: 50%;
+        top: 50%;
+        transform: translateX(-50%) translateY(-50%);
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
+        background-color: #f8f8f8;
+        overflow: hidden;
+    }
 
-//Write to server socket
-$message = "Hello!";
-$write = socket_write($socket, $message, strlen($message));
-if(!$write) {
-    echo "socket_write() Fail to connect:".socket_strerror($write)."\n";
-}
+    .top_menu {
+        background-color: #fff;
+        width: 100%;
+        padding: 20px 0 15px;
+        box-shadow: 0 1px 30px rgba(0, 0, 0, 0.1);
+    }
+    .top_menu .buttons {
+        margin: 3px 0 0 20px;
+        position: absolute;
+    }
+    .top_menu .buttons .button {
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        display: inline-block;
+        margin-right: 10px;
+        position: relative;
+    }
+    .top_menu .buttons .button.close {
+        background-color: #f5886e;
+    }
+    .top_menu .buttons .button.minimize {
+        background-color: #fdbf68;
+    }
+    .top_menu .buttons .button.maximize {
+        background-color: #a3d063;
+    }
+    .top_menu .title {
+        text-align: center;
+        color: #bcbdc0;
+        font-size: 20px;
+    }
 
-//Read the response from the server
-$result = socket_read ($socket, 1024) or die("Could not read server response\n");
-if(!$result) {
-    echo "socket_read() Fail to connect:".socket_strerror($result)."\n";
-}
-echo "Reply From Server  :".$result;
+    .messages {
+        position: relative;
+        list-style: none;
+        padding: 20px 10px 0 10px;
+        margin: 0;
+        height: 347px;
+        overflow-x: hidden;
+        overflow-y: auto;
+    }
+    .messages .message {
+        clear: both;
+        overflow: hidden;
+        margin-bottom: 20px;
+        transition: all 0.5s linear;
+        opacity: 0;
+    }
+    .messages .message.left .avatar {
+        background-color: #f5886e;
+        float: left;
+    }
+    .messages .message.left .text_wrapper {
+        background-color: #ffe6cb;
+        margin-left: 20px;
+    }
+    .messages .message.left .text_wrapper::after, .messages .message.left .text_wrapper::before {
+        right: 100%;
+        border-right-color: #ffe6cb;
+    }
+    .messages .message.left .text {
+        color: #c48843;
+    }
+    .messages .message.right .avatar {
+        background-color: #fdbf68;
+        float: right;
+    }
+    .messages .message.right .text_wrapper {
+        background-color: #c7eafc;
+        margin-right: 20px;
+        float: right;
+    }
+    .messages .message.right .text_wrapper::after, .messages .message.right .text_wrapper::before {
+        left: 100%;
+        border-left-color: #c7eafc;
+    }
+    .messages .message.right .text {
+        color: #45829b;
+    }
+    .messages .message.appeared {
+        opacity: 1;
+    }
+    .messages .message .avatar {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        display: inline-block;
+    }
+    .messages .message .text_wrapper {
+        display: inline-block;
+        padding: 20px;
+        border-radius: 6px;
+        width: calc(100% - 85px);
+        min-width: 100px;
+        position: relative;
+    }
+    .messages .message .text_wrapper::after, .messages .message .text_wrapper:before {
+        top: 18px;
+        border: solid transparent;
+        content: " ";
+        height: 0;
+        width: 0;
+        position: absolute;
+        pointer-events: none;
+    }
+    .messages .message .text_wrapper::after {
+        border-width: 13px;
+        margin-top: 0px;
+    }
+    .messages .message .text_wrapper::before {
+        border-width: 15px;
+        margin-top: -2px;
+    }
+    .messages .message .text_wrapper .text {
+        font-size: 18px;
+        font-weight: 300;
+    }
 
-// Close the socket
-socket_close($socket);
+    .bottom_wrapper {
+        position: relative;
+        width: 100%;
+        background-color: #fff;
+        padding: 20px 20px;
+        position: absolute;
+        bottom: 0;
+    }
+    .bottom_wrapper .message_input_wrapper {
+        display: inline-block;
+        height: 50px;
+        border-radius: 25px;
+        border: 1px solid #bcbdc0;
+        width: calc(100% - 160px);
+        position: relative;
+        padding: 0 20px;
+    }
+    .bottom_wrapper .message_input_wrapper .message_input {
+        border: none;
+        height: 100%;
+        box-sizing: border-box;
+        width: calc(75% - 40px);
+        position: absolute;
+        outline-width: 0;
+        color: gray;
+        left: 25%;
+        border-left: solid 1px #bcbdc0;
+        padding-left: 10px;
+    }
+    .bottom_wrapper .message_input_wrapper .name_input {
+        border: none;
+        height: 100%;
+        box-sizing: border-box;
+        width: calc(25% - 40px);
+        position: absolute;
+        outline-width: 0;
+        color: gray;
+    }
+    .bottom_wrapper .send_message {
+        width: 140px;
+        height: 50px;
+        display: inline-block;
+        border-radius: 50px;
+        background-color: #a3d063;
+        border: 2px solid #a3d063;
+        color: #fff;
+        cursor: pointer;
+        transition: all 0.2s linear;
+        text-align: center;
+        float: right;
+    }
+    .bottom_wrapper .send_message:hover {
+        color: #a3d063;
+        background-color: #fff;
+    }
+    .bottom_wrapper .send_message .text {
+        font-size: 18px;
+        font-weight: 300;
+        display: inline-block;
+        line-height: 48px;
+    }
+
+    .message_template {
+        display: none;
+    }
+
+    .system_error{
+        color: #bcbdc0;
+        font-weight: bold;
+        font-style: italic;
+    }
+
+    .system_msg{
+        color: #bcbdc0;
+        font-style: italic;
+    }
+
+
+</style>
+    <?
+    $colours = array('007AFF','FF7000','FF7000','15E25F','CFC700','CFC700','CF1100','CF00BE','F00');
+    $user_colour = $colours[array_rand($colours)];
+    ?>
+<script language="javascript" type="text/javascript">
+    $(document).ready(function(){
+
+            var Message;
+            Message = function (arg) {
+                this.text = arg.text, this.message_side = arg.message_side;
+                this.draw = function (_this) {
+                    return function () {
+                        var $message;
+                        $message = $($('.message_template').clone().html());
+                        $message.addClass(_this.message_side).find('.text').html(_this.text);
+                        $('.messages').append($message);
+                        return setTimeout(function () {
+                            return $message.addClass('appeared');
+                        }, 0);
+                    };
+                }(this);
+                return this;
+            };
+
+                var  sendMessage;
+
+                sendMessage = function (text, message_side) {
+                    var $messages, message;
+                    if (text.trim() === '') {
+                        return;
+                    }
+                    $('.message_input').val('');
+                    $messages = $('#message_box');
+                    message = new Message({
+                        text: text,
+                        message_side: message_side
+                    });
+                    message.draw();
+                    return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+                };
+
+
+
+
+        //create a new WebSocket object.
+        //var wsUri = "ws://localhost:9000/demo/server.php";
+        var wsUri = "ws://git.local:25003/server.php";
+        websocket = new WebSocket(wsUri);
+        websocket.onopen = function(ev) { // connection is open
+            $('#message_box').append("<div class=\"system_msg\">Connected!</div>"); //notify user
+        }
+        $('#send-btn').click(function(){ //use clicks message send button
+            var mymessage = $('#message').val(); //get message text
+            var myname = $('#name').val();; //get user name
+            if(myname == ""){ //empty name?
+                alert("Enter your Name please!");
+                return;
+            }
+            if(mymessage == ""){ //emtpy message?
+                alert("Enter Some message Please!");
+                return;
+            }
+            //prepare json data
+            var msg = {
+                message: mymessage,
+                name: myname,
+                color : '<?php echo $user_colour; ?>'
+            };
+            //convert and send data to server
+            websocket.send(JSON.stringify(msg));
+        });
+        //#### Message received from server?
+        websocket.onmessage = function(ev) {
+            var msg = JSON.parse(ev.data); //PHP sends Json data
+            var type = msg.type; //message type
+            var umsg = msg.message; //message text
+            var uname = msg.name; //user name
+            var ucolor = msg.color; //color
+            var ualign = msg.align; //color
+
+            if(type == 'usermsg')
+            {
+                sendMessage(umsg, ualign);
+            }
+            if(type == 'system')
+            {
+                $('#message_box').append("<div class=\"system_msg\">"+umsg+"</div>");
+            }
+            $('#message').val(''); //reset text
+        };
+
+        websocket.onerror	= function(ev){
+            var err = '';
+            if(!ev.data) err ='Could not connect to server!';
+            else err = ev.data;
+
+            $('#message_box').append("<div class=\"system_error\">Error Occurred - "+err+"</div>");
+        };
+
+        websocket.onclose 	= function(ev){$('#message_box').append("<div class=\"system_msg\">Connection Closed</div>");};
+    });
+</script>
+</head>
+<body>
+<div class="chat_window">
+    <div class="top_menu">
+        <div class="buttons">
+            <div class="button close"></div>
+            <div class="button minimize"></div>
+            <div class="button maximize"></div>
+        </div>
+        <div class="title">Chat</div>
+    </div>
+    <ul class="messages" id="message_box"></ul>
+    <div class="bottom_wrapper clearfix">
+        <div class="message_input_wrapper">
+            <input class="name_input"  id="name" placeholder="Type your name" style="color:#<?=$user_colour?>"/>
+            <input class="message_input"  id="message" placeholder="Type your message"/>
+        </div>
+        <div class="send_message">
+            <div class="icon"></div>
+            <div class="text"  id="send-btn">Send</div>
+        </div>
+    </div>
+</div>
+<div class="message_template"><li class="message"><div class="avatar"></div><div class="text_wrapper"><div class="text"></div></div></li></div>
+</body>
+</html>
--- server.php
+++ server.php
@@ -1,50 +1,186 @@
-<?php
-// no Time Limit
+<?
+// No Timeout
 set_time_limit(0);
 
-//Set variables such as "host" and "port"
-$host = '127.0.0.1';
-$port = 5353; // Port number can be any positive integer between 1024 -65535.
+/* Set variables "host" and "port" */
+$host = '127.0.0.1'; //host
+/* open port firewall windows 10 https://pureinfotech.com/open-port-firewall-windows-10/  */
+$port = '25003'; //port, Port number can be any positive integer between 1024 -65535.
 
-// Create socket
-$socket = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
-if(!$socket) {
-    echo "socket_create() Fail to create:".socket_strerror($socket)."\n";
+try {
+
+    //Create Socket
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    if(!is_resource($socket)){
+        throw new Exception('Unable to create socket: '.socket_strerror(socket_last_error($socket)));
+    }
+
+    // Reports whether local addresses can be reused.
+    if(!socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1)){
+        throw new Exception('Unable to set option on socket: '.socket_strerror(socket_last_error($socket)));
+    }
+
+    //Bind the socket to port and host
+    if(socket_bind($socket, 0, $port) === false){
+        throw new Exception('Unable to bind socket: '.socket_strerror(socket_last_error($socket)));
+    }
+
+    $setOpt = socket_get_option($socket, SOL_SOCKET, SO_REUSEADDR);
+    if ($setOpt === false) {
+        throw new Exception( 'Unable to get socket option: '.socket_strerror(socket_last_error($socket)));
+    }
+
+    //Start listening to the socket
+    if(socket_listen($socket) === false){
+        throw new Exception( 'Unable to to listen socket: '.socket_strerror(socket_last_error($socket)));
+    }
+
+    //Create for multiple Client
+    $clients = array($socket);
+
+    $write = NULL; //null var
+    $except = NULL; //null var
+
+    //Start endless loop from for Client sockets
+    while (true) {
+
+        //Manage multiple Client
+        $changed = $clients;
+
+        //Get a list of all the socket resources in $changed array that have data to be read from
+        $changed_sockets = socket_select($changed, $write, $except, 0, 10);
+        if ($changed_sockets === false) {
+            throw new Exception( 'Unable to socket select: '.socket_strerror(socket_last_error($socket)));
+        }
+
+        //Check for new socket
+        if (in_array($socket, $changed)) {
+
+            $socket_new = socket_accept($socket); //Accept incoming connection
+
+            $clients[] = $socket_new; //Add socket to client array
+
+            $header = socket_read($socket_new, 1024); //Read the message from the Client socket
+
+            perform_handshaking($header, $socket_new, $host, $port); //Perform websocket handshake
+
+            socket_getpeername($socket_new, $ip); //Get ip address of connected socket
+
+            $response = mask(json_encode(array('type' => 'system', 'message' => $ip . ' connected'))); //Prepare json data
+
+            send_message($response); //Notify all client about new client
+
+            //Make room for new socket
+            $found_socket = array_search($socket, $changed);
+            unset($changed[$found_socket]);
+        }
+
+        //Loop through all connected sockets
+        foreach ($changed as $c => $changed_socket) {
+
+            //Check for Receives data from a connected socket
+            while (socket_recv($changed_socket, $buf, 1024, 0) >= 1) {
+                $received_text = unmask($buf); //unmask data
+                $tst_msg = json_decode($received_text); //json decode
+                $user_name = $tst_msg->name; //sender name
+                $user_message = $tst_msg->message; //message text
+                $user_color = $tst_msg->color; //color
+                $user_align = ($c%2)?'left':'right'; //left or right
+
+                //prepare data to be sent to client
+                $response_text = mask(json_encode(array('type' => 'usermsg', 'name' => $user_name, 'message' => $user_message, 'color' => $user_color, 'align' => $user_align)));
+                send_message($response_text); //send data
+                break 2; //exist this loop
+            }
+
+            //Check disconnected socket
+            $buf = socket_read($changed_socket, 1024, PHP_NORMAL_READ);
+            if ($buf === false) {
+
+                //Remove client for $clients array
+                $found_socket = array_search($changed_socket, $clients);
+                socket_getpeername($changed_socket, $ip);
+                unset($clients[$found_socket]);
+
+                //Notify all users about disconnected connection
+                $response = mask(json_encode(array('type' => 'system', 'message' => $ip . ' disconnected')));
+                send_message($response);
+            }
+        }
+    }
+// close the listening socket
+    socket_close($socket);
+
+}catch (Exception $e){
+    echo $e->getMessage();
 }
 
-// Bind the socket to port and host
-$result = socket_bind($socket,$host,$port);
-if(!$result) {
-    echo "socket_bind() Fail to bind:".socket_strerror($result)."\n";
+function send_message($msg)
+{
+    global $clients;
+    foreach ($clients as $changed_socket) {
+        //Send message to the client socket
+        @socket_write($changed_socket, $msg, strlen($msg));
+    }
+    return true;
+}
+//Unmask incoming framed message
+function unmask($text) {
+    $length = ord($text[1]) & 127;
+    if($length == 126) {
+        $masks = substr($text, 4, 4);
+        $data = substr($text, 8);
+    }
+    elseif($length == 127) {
+        $masks = substr($text, 10, 4);
+        $data = substr($text, 14);
+    }
+    else {
+        $masks = substr($text, 2, 4);
+        $data = substr($text, 6);
+    }
+    $text = "";
+    for ($i = 0; $i < strlen($data); ++$i) {
+        $text .= $data[$i] ^ $masks[$i%4];
+    }
+    return $text;
+}
+//Encode message for transfer to client.
+function mask($text)
+{
+    $b1 = 0x80 | (0x1 & 0x0f);
+    $length = strlen($text);
+
+    if($length <= 125)
+        $header = pack('CC', $b1, $length);
+    elseif($length > 125 && $length < 65536)
+        $header = pack('CCn', $b1, 126, $length);
+    elseif($length >= 65536)
+        $header = pack('CCNN', $b1, 127, $length);
+    return $header.$text;
 }
 
-// Start listening to the socket
-$result = socket_listen($socket,4);
-if(!$result) {
-    echo "socket_listen() Fail to listen:".socket_strerror($result)."\n";
+//handshake new client.
+function perform_handshaking($receved_header,$client_conn, $host, $port)
+{
+    $headers = array();
+    $lines = preg_split("/\r\n/", $receved_header);
+    foreach($lines as $line)
+    {
+        $line = chop($line);
+        if(preg_match('/\A(\S+): (.*)\z/', $line, $matches))
+        {
+            $headers[$matches[1]] = $matches[2];
+        }
+    }
+    $secKey = $headers['Sec-WebSocket-Key'];
+    $secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
+    //hand shaking header
+    $upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
+        "Upgrade: websocket\r\n" .
+        "Connection: Upgrade\r\n" .
+        "WebSocket-Origin: $host\r\n" .
+        "WebSocket-Location: ws://$host:$port/demo/shout.php\r\n".
+        "Sec-WebSocket-Accept:$secAccept\r\n\r\n";
+    socket_write($client_conn,$upgrade,strlen($upgrade));
 }
-
-// Accept incoming connection
-$spawn =  socket_accept($socket);
-if(!$spawn) {
-    echo "socket_accept() Fail to accept:".socket_strerror($spawn)."\n";
-}
-
-// Read the message from the Client socket
-$input = socket_read($spawn, 1024);
-if(!$input) {
-    echo "socket_read() Fail to read:".socket_strerror($input)."\n";
-}
-
-// Reverse the message
-$output  = strrev($input);
-
-// Send message to the client socket
-$write = socket_write($spawn, $output, strlen ($output));
-if(!$input) {
-    echo "socket_write() Fail to write:".socket_strerror($write)."\n";
-}
-
-// Close the socket
-socket_close($spawn);
-socket_close($socket);
